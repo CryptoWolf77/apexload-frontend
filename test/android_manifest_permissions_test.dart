0a0063f873cd94f432e8040a44caf5e8f194f445
CryptoWolf77 <yhadrami2003@gmail.com>
2026-08-11 19:35:43 +0400

    Remove unnecessary Android media permissions

---
+++ test/android_manifest_permissions_test.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Android manifest removes dependency-injected broad media access', () {
+    final manifest = File(
+      'android/app/src/main/AndroidManifest.xml',
+    ).readAsStringSync();
+
+    expect(
+      manifest,
+      contains('xmlns:tools="http://schemas.android.com/tools"'),
+    );
+
+    for (final permission in const [
+      'android.permission.READ_MEDIA_IMAGES',
+      'android.permission.READ_MEDIA_VIDEO',
+      'android.permission.READ_MEDIA_AUDIO',
+      'android.permission.READ_EXTERNAL_STORAGE',
+    ]) {
+      final declaration = RegExp(
+        '<uses-permission\\s+'
+        '[^>]*android:name="$permission"'
+        '[^>]*tools:node="remove"\\s*/>',
+        multiLine: true,
+      );
+      expect(
+        manifest,
+        matches(declaration),
+        reason: '$permission must be removed during Android manifest merging.',
+      );
+    }
+
+    for (final permission in const [
+      'android.permission.WRITE_EXTERNAL_STORAGE',
+      'android.permission.MANAGE_EXTERNAL_STORAGE',
+      'android.permission.ACCESS_MEDIA_LOCATION',
+      'android.permission.READ_MEDIA_VISUAL_USER_SELECTED',
+      'com.google.android.gms.permission.AD_ID',
+    ]) {
+      expect(
+        manifest,
+        isNot(contains(permission)),
+        reason: '$permission must not be declared by ApexLoad.',
+      );
+    }
+  });
+}
